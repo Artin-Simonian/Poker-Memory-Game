a84317c6a03dbdbee223ed073e303c2fbd5d53d3
Artin-Simonian <artinsimonian@yahoo.com>
2023-08-16 13:49:41 -0700

    working on finalizing the functionallity of the game

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
-function cardsClicker(card){
-    card.className = 'show-cards'
+let cardOne = null;
+let cardTwo = null;
+const cardImgs = document.querySelectorAll(".hide-cards");
+
+// function to flip the cards
+// when being clicked
+function cardsClicker(card) {
+  if (cardOne == null) {
+    card.className = "show-cards";
+    cardOne = card;
+  } else if (cardTwo == null) {
+    card.className = "show-cards";
+    cardTwo = card;
+
+    setTimeout("matchingCards()", 1000);
+  }
+}
+
+
+cardImgs.forEach((card) => {
+  card.addEventListener("click", () => cardsClicker(card));
+});
+
+function matchingCards() {
+  if (cardOne.src == cardTwo.src) {
+    cardOne = null;
+    cardTwo = null;
+  } else {
+    cardOne.className = "hide-cards";
+    cardTwo.className = "hide-cards";
+    cardOne = null;
+    cardTwo = null;
+  }
 }
