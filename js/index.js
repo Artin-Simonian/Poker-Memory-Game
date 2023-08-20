d5ec9fc79f751d6a8001a65074e1c027c7f74a11
Artin-Simonian <artinsimonian@yahoo.com>
2023-08-20 15:12:11 -0700

    pushing README.MD to github

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-const audio = new Audio('/audio/casino.mp3');
+const audio = new Audio("/audio/casino.mp3");
+const wrongCardAudio = new Audio("/audio/correct.mp3");
+const audience = new Audio("/audio/audience.mp3");
+const jackPot = new Audio("/audio/jackpot.mp3");
 
 let cardOne = null;
 let cardTwo = null;
-let moves = 0;
+let moves = 20;
 
 let cardImgs = document.querySelectorAll(".hide-cards");
 let resetBtn = document.querySelector("button");
-const movesEl = document.getElementById('moves-count');
+const movesEl = document.getElementById("moves-count");
 
 const imageContainer = document.querySelector("#container");
 const images = document.querySelectorAll(".hide-cards");
@@ -16,6 +19,8 @@ function shuffleArray(array) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
+  audio.currentTime = 0;
+  audio.play();
 }
 
 const shuffledImages = Array.from(images);
@@ -36,8 +41,11 @@ function cardsClicker(card) {
     cardTwo = card;
 
     setTimeout("matchingCards()", 1000);
-    moves++;
+    moves--;
     movesEl.textContent = moves;
+    if(moves === 0){
+      location.reload();
+    }
   }
 }
 
@@ -49,6 +57,13 @@ function matchingCards() {
   if (cardOne.src == cardTwo.src) {
     cardOne = null;
     cardTwo = null;
+    wrongCardAudio.play();
+
+    if (areAllCardsFlipped()) {
+      audio.pause();
+      audience.play();
+      jackPot.play();
+    }
   } else {
     cardOne.className = "hide-cards";
     cardTwo.className = "hide-cards";
@@ -57,8 +72,12 @@ function matchingCards() {
   }
 }
 
+function areAllCardsFlipped() {
+  const flippedCards = document.querySelectorAll(".show-cards");
+  return flippedCards.length === cardImgs.length;
+}
+
+
 resetBtn.addEventListener("click", () => {
   location.reload();
-  audio.currentTime = 0;
-  audio.play();
 });
