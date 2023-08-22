eea3c493678638c749bd62606debce70c6b80f81
Artin-Simonian <artinsimonian@yahoo.com>
2023-08-21 21:03:27 -0700

    finalizing function and variable name

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
+// Audio variables
 const audio = new Audio("/audio/casino.mp3");
 const wrongCardAudio = new Audio("/audio/correct.mp3");
 const audience = new Audio("/audio/audience.mp3");
 const jackPot = new Audio("/audio/jackpot.mp3");
 
+// card and button variables
 let cardOne = null;
 let cardTwo = null;
 let moves = 20;
-
 let cardImgs = document.querySelectorAll(".hide-cards");
 let resetBtn = document.querySelector("#button");
 const movesEl = document.getElementById("moves-count");
-
 const imageContainer = document.querySelector("#container");
 const images = document.querySelectorAll(".hide-cards");
 
-function shuffleArray(array) {
+// Creates board and shuffles cards
+function shuffleBoard(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -23,24 +24,23 @@ function shuffleArray(array) {
   audio.play();
 }
 
+// creates a new array from the 'images'
 const shuffledImages = Array.from(images);
-shuffleArray(shuffledImages);
+shuffleBoard(shuffledImages);
 
 shuffledImages.forEach((image) => {
   imageContainer.appendChild(image);
 });
 
-// function to flip the cards
-// when being clicked
-function cardsClicker(card) {
+// handle click function
+function handleClick(card) {
   if (cardOne == null) {
     card.className = "show-cards";
     cardOne = card;
   } else if (cardTwo == null) {
     card.className = "show-cards";
     cardTwo = card;
-
-    setTimeout("matchingCards()", 1000);
+    setTimeout("cardsMatched()", 1000);
     moves--;
     movesEl.textContent = moves;
     if (moves === 0) {
@@ -50,16 +50,16 @@ function cardsClicker(card) {
 }
 
 cardImgs.forEach((card) => {
-  card.addEventListener("click", () => cardsClicker(card));
+  card.addEventListener("click", () => handleClick(card));
 });
 
-function matchingCards() {
+function cardsMatched() {
   if (cardOne.src == cardTwo.src) {
     cardOne = null;
     cardTwo = null;
     wrongCardAudio.play();
 
-    if (areAllCardsFlipped()) {
+    if (winner()) {
       audio.pause();
       audience.play();
       jackPot.play();
@@ -72,7 +72,7 @@ function matchingCards() {
   }
 }
 
-function areAllCardsFlipped() {
+function winner() {
   const flippedCards = document.querySelectorAll(".show-cards");
   return flippedCards.length === cardImgs.length;
 }
